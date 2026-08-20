cf1ad88d34ef3c3c8c2835e1955b3dad8ee70b17
daniil.chekov <Chekov.99@mail.ru>
2026-08-20 15:17:50 +0700

    Deploy Taxi-Vezi PWA from 6ce6e3c

---
+++ assets/maplibre-gl-worker-CRJEFZwx.js
@@ -0,0 +1,1 @@
+const e="/Taxi-Vezi-Pages/assets/maplibre-gl-worker-DXBASRqI.js";export{e as default};
--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const BUILD_ID = 'Cd67SWR4';
+const BUILD_ID = 'Cs8hPBNN';
 const CACHE_NAME = `taxi-vezi-pages-${BUILD_ID}`;
 const APP_SCOPE = '/Taxi-Vezi-Pages/';
 const APP_SHELL = [APP_SCOPE, `${APP_SCOPE}manifest.json`, `${APP_SCOPE}pwa-icon.svg`, `${APP_SCOPE}pwa-splash.svg`];
