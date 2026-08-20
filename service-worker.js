52c4ae3ceba4ec88a160af5b646c029bbf89be33
daniil.chekov <Chekov.99@mail.ru>
2026-08-21 01:16:44 +0700

    build: publish shell fix from 97b4c64

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const BUILD_ID = 'BEs9yJxm';
+const BUILD_ID = 'BjtEQh0v';
 const CACHE_NAME = `taxi-vezi-pages-${BUILD_ID}`;
 const APP_SCOPE = '/Taxi-Vezi-Pages/';
 const APP_SHELL = [APP_SCOPE, `${APP_SCOPE}manifest.json`, `${APP_SCOPE}pwa-icon.svg`, `${APP_SCOPE}pwa-splash.svg`];
