8ec2b1a3361983f58f0176eebe25276d758ca586
daniil.chekov <Chekov.99@mail.ru>
2026-08-21 00:52:16 +0700

    build: enable Pages design preview from a50ff94

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const BUILD_ID = 'DW7hXbOK';
+const BUILD_ID = 'BEs9yJxm';
 const CACHE_NAME = `taxi-vezi-pages-${BUILD_ID}`;
 const APP_SCOPE = '/Taxi-Vezi-Pages/';
 const APP_SHELL = [APP_SCOPE, `${APP_SCOPE}manifest.json`, `${APP_SCOPE}pwa-icon.svg`, `${APP_SCOPE}pwa-splash.svg`];
